10d32baa70059adb900801f138a099bc53a6c543
szidonia.laszlo <szidonia.laszlo@directtwo.hu>
2019-12-18 10:42:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/szidonialaszlo/screenlock2project/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.szidonialaszlo.screenlock2project;
+
+
+import android.os.Bundle;
+
+public class MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
